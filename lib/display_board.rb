1b3baaf312c4074b72f72967ffeb3bdd5b124b27
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-04 19:42:24 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,16 @@
 # out the current state.
 def display_board(board)
 
-  puts = board[0], "|" ,board[1], "|" , board[2]
-  puts = "-----------"
-  puts = board[3], "|" ,board[4], "|" , board[5]
-  puts = "-----------"
-  puts = board[6], "|" ,board[7], "|" , board[8]
+  row_one = board[0], "|" ,board[1], "|" , board[2]
+  row_two = "-----------"
+  row_three = board[3], "|" ,board[4], "|" , board[5]
+  row_four = "-----------"
+  row_five = board[6], "|" ,board[7], "|" , board[8]
+
+  puts row_one
+  puts row_two
+  puts row_three
+  puts row_four
+  puts row_five
 
 end
